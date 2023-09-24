78ea11da709c7d699dda787875e41965251fab7b
qq <linbaz2613459870@gmail.com>
2023-09-25 00:23:07 +0300

    destroy arrow after sec

--- TestGame/Assets/Assets/Scripts/ArrowBullet.cs
+++ TestGame/Assets/Assets/Scripts/ArrowBullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ArrowBullet : MonoBehaviour
@@ -7,11 +8,18 @@ public class ArrowBullet : MonoBehaviour
     public float buletSpeed;
     private Rigidbody2D rb;
     public ToWeapon tw;
-
+    private int destroy = 1;
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right*buletSpeed;
+        Invoke("DestroyTime", destroy);
+    }
+
+    void DestroyTime()
+    {
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)

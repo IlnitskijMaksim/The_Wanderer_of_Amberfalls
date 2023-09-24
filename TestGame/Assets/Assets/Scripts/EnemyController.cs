465fdb7c6b577571b548d256cac5f01c80f3fb49
qq <linbaz2613459870@gmail.com>
2023-09-24 19:40:19 +0300

    clever enemy(AI)

--- TestGame/Assets/Assets/Scripts/EnemyController.cs
+++ TestGame/Assets/Assets/Scripts/EnemyController.cs
@@ -1,51 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public string playerTag = "Player"; // Тег игрока
-    public float detectionRadius = 5f; // Радиус обнаружения игрока
-    public float moveSpeed = 2f; // Скорость движения slime
+    [SerializeField] Transform target;
 
-    private Transform player;
-    private bool isPlayerInRange = false;
-
+    NavMeshAgent agent;
     private void Start()
     {
-        // Найти игрока по тегу при старте
-        player = GameObject.FindGameObjectWithTag(playerTag).transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     private void Update()
     {
-        if (player == null)
-        {
-            // Игрок не найден, выходим
-            return;
-        }
-
-        // Проверяем расстояние между slime и игроком
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-
-        // Если игрок находится в радиусе обнаружения
-        if (distanceToPlayer <= detectionRadius)
-        {
-            isPlayerInRange = true;
-        }
-        else
-        {
-            isPlayerInRange = false;
-        }
-
-        // Если игрок в радиусе обнаружения, двигаем slime к игроку
-        if (isPlayerInRange)
-        {
-            // Вычисляем направление движения к игроку
-            Vector2 direction = (player.position - transform.position).normalized;
-
-            // Двигаем slime в направлении игрока
-            transform.Translate(direction * moveSpeed * Time.deltaTime);
-        }
+        agent.SetDestination(target.position);
     }
-
-
 }

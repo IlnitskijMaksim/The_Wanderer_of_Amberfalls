ae44216a4e33294db4ae3173d636e3ea663cca26
Ільницький Максим <112692170+IlnitskijMaksim@users.noreply.github.com>
2023-12-05 15:45:38 +0200

    added ranged enemy prototype

---
+++ TestGame/Assets/Assets/Scripts/Enemy/EnemyShoot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+
+            Destroy(gameObject);
+        }
+    }
+}
